451dff995aa20df069dc0c57c88a9c710bef1c1c
Nikita M. <slimnik47@gmail.com>
2024-07-30 13:55:52 +0300

    Try Form and Query types

--- app/main.py
+++ app/main.py
@@ -1,18 +1,19 @@
-from datetime import datetime
-from typing import Optional
-
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Form
 
 
 app = FastAPI()
 
 
-@app.get("/hotels/{hotel_name}/{hotel_id}")
-def get_hotels(hotel_id: int,
-               hotel_name: str,
-               date_from: datetime,
-               date_to: datetime,
-               stars: Optional[int] = Query(None, gt=0, lt=6),
-               sea_view: Optional[bool] = None
-               ):
-    return hotel_id, hotel_name, stars
+# @app.get("/hotels/{hotel_name}/{hotel_id}")
+# def get_hotels(hotel_id: int,
+#                hotel_name: str,
+#                date_from: datetime,
+#                date_to: datetime,
+#                stars: Optional[int] = Query(None, gt=0, lt=6),
+#                sea_view: Optional[bool] = None
+#                ):
+#     return hotel_id, hotel_name, stars
+
+@app.get('/calculate')
+def get_page(num1: int = Form(ge=0, le=12), num2: int = Query(int, ge=0, le=100)):
+    return {"result": num1 + num2}
