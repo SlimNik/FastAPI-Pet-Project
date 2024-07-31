4614df2e3879b44142dc5349c7c36cb97d23401f
Nikita M. <slimnik47@gmail.com>
2024-07-31 15:03:29 +0300

    Create Alembic integration. New project structure

--- app/database.py
+++ app/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
+from app.config import settings
 
-DB_URL = "postgresql+asyncpg://postgres:admin@localhost:5432/shopdb"
-engine = create_async_engine(url=DB_URL)
+
+engine = create_async_engine(url=settings.DB_URL)
 
 session = async_sessionmaker(engine, expire_on_commit=False)
 
