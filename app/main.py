b23d1b4edaaa70834c72a10e5f2d7a4a36972a79
Nikita M. <slimnik47@gmail.com>
2024-08-05 11:43:02 +0300

    Add users auth logic

--- app/main.py
+++ app/main.py
@@ -5,10 +5,13 @@ from fastapi import FastAPI, Query, Depends
 from pydantic import BaseModel
 
 from app.bookings.router import router as router_bookings
+from app.users.router import router as router_users
 
 
 app = FastAPI()
 
+# порядок подключения роутеров влияет на их порядок в документации
+app.include_router(router_users)
 app.include_router(router_bookings)
 
 
