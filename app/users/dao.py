993c02e091fb77436e4be6d1a269ef310b245bd3
Nikita <slimnik47@gmail.com>
2024-08-10 11:02:35 +0300

    Bug fix №1

--- app/users/dao.py
+++ app/users/dao.py
@@ -11,13 +11,13 @@ class UsersDAO(BaseDAO):
     @classmethod  # использум метод класса, чтобы каждый раз не создавать новый объект
     async def get_all(cls):
         async with async_session() as session:
-            query = select(UserModel.id.label('user_id'), UserModel.email)
+            query = select(UserModel.id, UserModel.email)
             result = await session.execute(query)
             return result.mappings().all()
 
     @classmethod
     async def get_by_id(cls, user_id: int):
         async with async_session() as session:
-            query = select(UserModel.id.label('user_id'), UserModel.email).filter_by(id=user_id)
+            query = select(UserModel.id, UserModel.email).filter_by(id=user_id)
         result = await session.execute(query)
         return result.mappings().one()
